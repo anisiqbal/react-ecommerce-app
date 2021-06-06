eeba0e47da3bbc2deb5ffd366a9effcc42766883
Anis Iqbal <anisiqbal484@gmail.com>
2021-06-06 15:50:18 +0500

    props with child component done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,26 @@
+import React, { useState } from 'react';
 import Card from '../shared/Card';
 
 function Home() {
+    const [products, setProduct] = useState([
+        { Id: 1, name: 'Apple Watch', price: 55000, description: '44mm or 40mm case size Always-On Retina display' },
+        { Id: 2, name: 'Running Shoe', price: 3000, description: 'This is a genuine product of ASICS India Pvt. Ltd.' },
+        { Id: 3, name: 'Striped Men Round Neck', price: 2500, description: 'This is a genuine product of ASICS India Pvt. Ltd.' },
+        { Id: 4, name: 'SAF Digital Reprint 9.5', price: 55000, description: 'This is a genuine product of ASICS India Pvt. Ltd.' },
+    ]);
+    
     return(
-        <div className="container py-5">
+        <div className="container">
             <div className="row">
-                <div className="col-md-3">
-                    <Card />
-                </div>
-                <div className="col-md-3">
-                    <Card />
-                </div>
-                <div className="col-md-3">
-                    <Card />
-                </div>
-                <div className="col-md-3">
-                    <Card />
-                </div>
+                {
+                    products.map((item, i) => {
+                        return(
+                            <div className="col-md-3 mt-3">
+                                <Card data={item} />
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     );
--- src/shared/Card.js
+++ src/shared/Card.js
@@ -5,8 +5,9 @@ function Card(props) {
         <div class="card">
             <img src={product} class="card-img-top" alt="product" />
             <div class="card-body">
-                <h5 class="card-title">Apple Watch</h5>
-                <p class="card-text">44mm or 40mm case size Always-On Retina display</p>
+                <h5 class="card-title text-truncate">{props.data.name}</h5>
+                <p>PKR. <strong>{props.data.price}</strong></p>
+                <p class="card-text">{props.data.description}</p>
                 <a href="#" class="btn btn-primary">Add to Cart</a>
             </div>
         </div>
