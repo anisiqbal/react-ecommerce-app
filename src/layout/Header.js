ced199086aa3e21f218dab5f4ba6184a812a8a31
Anis Iqbal <anisiqbal484@gmail.com>
2021-06-06 14:56:30 +0500

    header donw

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,6 +1,32 @@
+import '../assets/css/header.css';
+import logo from '../assets/img/logo.png';
+
 function Header() {
     return(
-        <h1>Header</h1>
+        <nav class="navbar navbar-dark bg-primary">
+            <div className="container">
+                <div className="row w-100">
+                    <div className="col-md-3">
+                        <a className="logo">
+                            <img src={logo} />
+                        </a>
+                    </div>
+                    <div className="col-md-9">
+                        <ul>
+                            <li>
+                                <a>Home</a>
+                            </li>
+                            <li>
+                                <a>About</a>
+                            </li>
+                            <li>
+                                <a>Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 }
 
