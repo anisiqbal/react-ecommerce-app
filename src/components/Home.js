33081ef0d3d31ec40aa2a02a741b614e44b5886c
Anis Iqbal <anisiqbal484@gmail.com>
2021-06-06 14:42:21 +0500

    component structuring

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+function Home() {
+    return(
+        <h1>Hello from Homepage</h1>
+    );
+}
+
+export default Home;
---
+++ src/shared/Card.js
@@ -0,0 +1,7 @@
+function Card(props) {
+    return(
+        <h1>Hello from Card</h1>
+    );
+}
+
+export default Card;
